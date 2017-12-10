ae29ea8f2868694e2e909cf15a46c108a4f51bd9
VladimirChertilov <vladimir.chertilov@epam.com>
2017-12-10 23:06:01 +0300

    removed Maybe.just

--- repository/src/main/java/com/theapp/repository/Repository.java
+++ repository/src/main/java/com/theapp/repository/Repository.java
@@ -7,8 +7,7 @@ import io.reactivex.Maybe;
 public class Repository {
 
     public Maybe<Hotel> getHotelById(long id) {
-        final Hotel hotel = new Hotel(1, "Bellagio", "Las-Vegas", 5.0d, 1000d,
-                                      "3.jpg", "55:77", 0, 0);
-        return Maybe.just(hotel);
+        return Maybe.fromCallable(() -> new Hotel(id, "Bellagio", "Las-Vegas", 5.0d, 1000d,
+                                      "3.jpg", "55:77", 0, 0));
     }
 }
